24d57b25e9ee8204402b406ab65a44aac6543234
jbbarth <jeanbaptiste.barth@gmail.com>
2013-07-23 19:51:16 +0200

    Inital commit
    Empty rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cartoque::Application.config.secret_key_base = '636e8e30749b2d3d9eddfd24b8192f3861f26e5cb151fd9cab79018c3d85b19b357bcd063c9c7ffc2259bcab708068fb2becef13a052e9104b7ad0bdf7febccf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cartoque::Application.config.session_store :cookie_store, key: '_cartoque_session'
