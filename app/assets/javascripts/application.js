100ac3769d729265a20163cd8c4699d99c5dc888
jbbarth <jeanbaptiste.barth@gmail.com>
2013-07-25 08:11:01 +0200

    Add twitter bootstrap and font-awesome for styling
    The goal is to make styling as standard as possible and mostly avoid
    specific hacks in every page / section, which turns out to be a real
    maintenance nightmare.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//
+//Loads all Bootstrap javascripts
+//= require bootstrap
+//
+//Loads every local js next
 //= require_tree .
