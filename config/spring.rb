3d3c0b08458f5e24c29ce6bc11de6d431f527d82
jbbarth <jeanbaptiste.barth@gmail.com>
2013-07-26 14:33:34 +0200

    Ensure rspec runs with a different order (seed) each time
    See: https://github.com/jonleighton/spring/issues/113

---
+++ config/spring.rb
@@ -0,0 +1,10 @@
+# generate random seed for rspec
+# see: https://github.com/jonleighton/spring/issues/113
+Spring.after_fork do
+  if Rails.env == 'test'
+    RSpec.configure do |config|
+      srand
+      config.seed = srand % 0xFFFF
+    end
+  end
+end
