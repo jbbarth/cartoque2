19ebedd9659c1f601973bbb3563add0eeda5f877
jbbarth <jeanbaptiste.barth@gmail.com>
2013-09-08 13:00:53 +0200

    Switch to postgresql for now

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130802124252) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "apps", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
@@ -44,6 +47,6 @@ ActiveRecord::Schema.define(version: 20130802124252) do
     t.string   "authentication_token"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
