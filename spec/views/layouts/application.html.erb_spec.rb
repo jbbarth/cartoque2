8353f899e3c88a882a01df6cee355372cbb5c54c
jbbarth <jeanbaptiste.barth@gmail.com>
2013-09-16 23:38:33 +0200

    Fix failing view test after previous commits

--- spec/views/layouts/application.html.erb_spec.rb
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -6,25 +6,25 @@ describe "layouts/application" do
   end
 
   it "displays notice if present" do
-    flash.now[:notice] = "A warning"
+    flash.now[:notice] = "A notice"
     render
-    assert_select "div.alert-warning", text: "A warning"
+    assert_select "div.flash-notice", text: "A notice"
   end
 
   it "doesn't show notice box if no notice" do
     render
-    assert_select "div.alert-warning", count: 0
+    assert_select "div.flash-notice", count: 0
   end
 
-  it "displays alert if present" do
-    flash.now[:alert] = "An alert"
+  it "displays flash if present" do
+    flash.now[:error] = "An alert"
     render
-    assert_select "div.alert-error", text: "An alert"
+    assert_select "div.flash-error", text: "An alert"
   end
 
-  it "doesn't show alert box if no alert" do
+  it "doesn't show flash box if no flash" do
     render
-    assert_select "div.alert-error", count: 0
+    assert_select "div.flash-error", count: 0
   end
 
   it "doesn't show links in navbar if not logged in" do
