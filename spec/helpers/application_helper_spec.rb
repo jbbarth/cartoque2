21bfe72ffc418939047d481e252c9e8fbf69eb07
jbbarth <jeanbaptiste.barth@gmail.com>
2013-09-23 23:11:24 +0200

    Remove non-used files in app/assets and app/helpers, and added an example helper spec for ApplicationHelper

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 # Specs in this file have access to a helper object that includes
-# the AppsHelper. For example:
+# the ApplicationHelper. For example:
 #
-# describe AppsHelper do
+# describe ApplicationHelper do
 #   describe "string concat" do
 #     it "concats two strings with spaces" do
 #       helper.concat_strings("this","that").should == "this that"
 #     end
 #   end
 # end
-describe AppsHelper do
+describe ApplicationHelper do
 end
