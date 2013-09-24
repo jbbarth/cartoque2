9361851657001fe2c654a2ccee89a089f49444f8
jbbarth <jeanbaptiste.barth@gmail.com>
2013-09-24 07:50:08 +0200

    Split application.js into multiple files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,52 +24,6 @@
 //= require_tree .
 
 $(function() {
-  //helper methods
-  function clickFirst(jQueryObject) {
-    jQueryObject.length && jQueryObject[0].click()
-  }
-
   //hide alert messages after 6s
   $('#flash').delay(6000).fadeOut(300)
-
-  //back to index when a show/edit window is open
-  key('esc', function(e) {
-    if ($('.show').length) {
-      clickFirst($('h1 a, ul.nav li.active a'))
-    }
-    if ($('.modal').length) {
-      $('.modal').modal('hide')
-    }
-  }) 
-  //shortcuts for some links
-  key('n', function(e) {
-    clickFirst($('#new'))
-  })
-  key('right', function(e) {
-    if ($('.nav-tabs li.active').length) {
-      var selectedTab = $('.nav-tabs li.active')
-      var nextTab = selectedTab.next('li')
-      if (!nextTab.length) { nextTab = $('.nav-tabs li').first() }
-      clickFirst(nextTab.find('a'))
-    }
-  })
-  key('left', function(e) {
-    if ($('.nav-tabs li.active').length) {
-      var selectedTab = $('.nav-tabs li.active')
-      var nextTab = selectedTab.prev('li')
-      if (!nextTab.length) { nextTab = $('.nav-tabs li').last() }
-      clickFirst(nextTab.find('a'))
-    }
-  })
-  //shortcuts help modal
-  //doesn't work in keymaster so... see: https://github.com/madrobby/keymaster/issues/59
-  $(document).on('keyup',function(e) {
-    var $target = $(e.target)
-    if (e.keyCode == 191 && !$target.is("input") && !$target.is("textarea") && !$target.is("select")) {
-      $.getScript("/help/keyboard")
-    }
-  })
-///  $(document).on('keyup', function(e) {
-///    console.log(e.keyCode)
-///  })
 })
---
+++ app/assets/javascripts/helpers.js
@@ -0,0 +1,6 @@
+// Helper methods available globally
+
+//clicks on the first object of the jquery collection if any
+function clickFirst(jQueryObject) {
+  jQueryObject.length && jQueryObject[0].click()
+}
